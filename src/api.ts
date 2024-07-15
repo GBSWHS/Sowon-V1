2ea93de5bb041f9907a8ace87de03cac51fa250b
806gw <shjea4364@naver.com>
2024-07-15 11:07:51 +0900

    feat: gpt 모델 변경 및 페이지 재구성

--- src/api.ts
+++ src/api.ts
@@ -1,25 +1,36 @@
 import OpenAI from "openai";
 
 const openai = new OpenAI({
-   apiKey: import.meta.env.VITE_OPENAI_API_KEY,
-   dangerouslyAllowBrowser: true,
+  apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+  dangerouslyAllowBrowser: true,
 });
 
 export async function getFineTunedResponse(prompt: string) {
-   try {
-      const response = await openai.chat.completions.create({
-         model: "ft:gpt-3.5-turbo-0125:personal::9jHsgCW2",
-         messages: [
-            { role: "user", content: prompt },
-         ],
-         max_tokens: 100,
-      });
-      if (response.choices && response.choices[0] && response.choices[0].message && response.choices[0].message.content) {
-         return(response.choices[0].message.content.trim());
-      } else {
-         return("Response is null or undefined");
-      }
-   } catch (error) {
-      console.error(error);
-   }
+  try {
+    const response = await openai.chat.completions.create({
+      model: import.meta.env.VITE_MODEL,
+      messages: [
+        {
+          role: "system",
+          content: "경소마고는 경북소프트웨어마이스터고등학교 입니다",
+        },
+        { role: "system", content: "저는 경소마고의 마스코트 소원이 입니다" },
+        { role: "user", content: prompt },
+      ],
+      max_tokens: 100,
+      temperature: 0.2,
+    });
+    if (
+      response.choices &&
+      response.choices[0] &&
+      response.choices[0].message &&
+      response.choices[0].message.content
+    ) {
+      return response.choices[0].message.content.trim();
+    } else {
+      return "Response is null or undefined";
+    }
+  } catch (error) {
+    console.error(error);
+  }
 }
--- src/page/Landing.tsx
+++ src/page/Landing.tsx
@@ -1,38 +1,68 @@
-import { useState } from 'react';
-import Logo from '../assets/logo.jpg';
+import React, { useState } from 'react';
+import Logo from '../assets/sowon.webp';
 import '../style/Landing.scss';
 import { getFineTunedResponse } from '../api';
+
+const Loading = () => (
+    <div className="typing-indicator">
+        <div className="typing-circle typing-circle-1"></div>
+        <div className="typing-circle typing-circle-2"></div>
+        <div className="typing-circle typing-circle-3"></div>
+        <div className="typing-shadow typing-shadow-1"></div>
+        <div className="typing-shadow typing-shadow-2"></div>
+        <div className="typing-shadow typing-shadow-3"></div>
+    </div>
+);
 
 const Landing = () => {
     const [prompt, setPrompt] = useState('');
     const [answer, setAnswer] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            handleSubmit(e);
+        }
+    };
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setPrompt(e.target.value);
     };
-    
-    const handleSubmit = async () => {
+
+    const handleSubmit = async (e: React.FormEvent | React.KeyboardEvent) => {
+        e.preventDefault();
+        setLoading(true);
+
         try {
             const response = await getFineTunedResponse(prompt);
             setAnswer(response ?? '');
+
         } catch (error) {
             console.error("에러 발생:", error);
+        } finally {
+            setLoading(false);
         }
     };
 
     return (
         <div className='background'>
             <div className='landing'>
-                <img src={Logo} alt="경소고 로고" className='logo white' /> <br />
-                <input
-                    placeholder="경북소프트웨어마이스터고등학교에 대해 궁금한게 있나요 ?"
-                    className='chat-input'
-                    value={prompt}
-                    onChange={handleInputChange}
-                />
-                <button onClick={handleSubmit}>Submit</button>
+                <img src={Logo} alt="경소고 로고" className='logo white' />
+                {loading ? <Loading /> : <p className='answer'>{answer}</p>}
+                <br />
+                <div className='input-container'>
+                    <div className='input-box'>
+                        <input
+                            placeholder="경북소프트웨어마이스터고등학교에 대해 궁금한게 있나요 ?"
+                            className='chat-input'
+                            value={prompt}
+                            onKeyDown={handleKeyDown}
+                            onChange={handleInputChange}
+                        />
+                    </div>
+                    <button onClick={handleSubmit}>🔍︎</button>
+                </div>
             </div>
-            <h4>{answer}</h4>
         </div>
     );
 };
